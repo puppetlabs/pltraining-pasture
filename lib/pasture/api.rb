b9a94f21756aebd9d166f0d68254ec0c11dd354d
kjhenner <kjhenner@gmail.com>
2017-04-20 13:48:13 -0700

    add sqlite support

--- lib/pasture/api.rb
+++ lib/pasture/api.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'cowsay'
+require 'pg'
+require 'sequel'
 
 module Pasture
 
@@ -7,13 +9,44 @@ module Pasture
 
     include Cowsay
 
+    def initialize
+      super()
+      @conn = settings.pasture_options[:db] ? Sequel.connect(settings.pasture_options[:db]) : Sequel.sqlite
+      unless @conn.table_exists?(:sayings)
+        @conn.create_table :sayings do
+          primary_key :id
+          String :message
+        end
+      end
+    end
+
     helpers do
       def options
         settings.pasture_options
       end
+      def conn
+        @conn ||= Sequel.connect(settings.pasture_options[:db])
+      end
+    end
+
+    post '/api/v1/cowsay/sayings' do
+      conn[:sayings].insert(:message => params[:message])
+    end
+
+    get '/api/v1/cowsay/sayings' do
+      content_type :json
+      conn[:sayings].all.to_json
+    end
+
+    get '/api/v1/cowsay/sayings/:id' do
+      content_type :json
+      message = conn[:sayings].where(:id => params[:id]).naked.first[:message]
+      Cowsay.say(message,
+                 params[:character] || options[:default_character])
     end
 
     get '/api/v1/cowsay' do
+      content_type :json
       Cowsay.say(params[:message] || options[:default_message] || '',
                  params[:character] || options[:default_character])
     end
